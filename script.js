60bc22727c245b4d9d16c6a1db1f12be9adde9b9
ShrAmix <ifoxps@gmail.com>
2025-02-06 21:57:46 +0200

    update

--- script.js
+++ script.js
@@ -121,3 +121,26 @@ document.querySelectorAll('.project-card').forEach(card => {
       });
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modalImage");
+  const closeBtn = document.querySelector(".close");
+
+  document.querySelectorAll(".screenshot").forEach(img => {
+      img.addEventListener("click", function () {
+          modal.style.display = "flex"; // Показати модальне вікно
+          modalImg.src = this.src; // Встановити джерело зображення
+      });
+  });
+
+  closeBtn.addEventListener("click", function () {
+      modal.style.display = "none"; // Приховати модальне вікно
+  });
+
+  // Закриття модального вікна при натисканні поза зображенням
+  modal.addEventListener("click", function (event) {
+      if (event.target === modal) {
+          modal.style.display = "none";
+      }
+  });
+});
